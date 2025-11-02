b458437f68a7141dc1473a694dc8ca7b11318e81
MirkaanStefanov <135588213+MirkaaStefanov@users.noreply.github.com>
2025-11-03 00:57:26 +0200

    remove delete user scheduler

--- src/main/java/com/example/Elit17Plus_backend/config/schedulers/UserCleanupScheduler.java
+++ src/main/java/com/example/Elit17Plus_backend/config/schedulers/UserCleanupScheduler.java
@@ -29,20 +29,20 @@ public class UserCleanupScheduler {
         this.verificationTokenRepository = verificationTokenRepository;
     }
 
-    @Scheduled(cron = "0 00 0 * * *") // Run every 24 hours
-    public void deleteUnconfirmedUsers() {
-        LocalDateTime thresholdDateTime = LocalDateTime.now().minusHours(24);
-
-        // Retrieve unconfirmed users created before the threshold date time
-        List<User> unconfirmedUsers = userRepository.findByEnabledFalseAndCreatedAtBeforeAndDeletedAtIsNull(thresholdDateTime);
-
-        for (User user : unconfirmedUsers) {
-            List<VerificationToken> userVerificationTokens = verificationTokenRepository.findByUserAndCreatedAtBefore(user, thresholdDateTime);
-            verificationTokenRepository.deleteAll(userVerificationTokens);
-
-            List<Token> userTokens = tokenRepository.findAllByUser(user);
-            tokenRepository.deleteAll(userTokens);
-            userRepository.deleteAll(unconfirmedUsers);
-        }
-    }
+//    @Scheduled(cron = "0 00 0 * * *") // Run every 24 hours
+//    public void deleteUnconfirmedUsers() {
+//        LocalDateTime thresholdDateTime = LocalDateTime.now().minusHours(24);
+//
+//        // Retrieve unconfirmed users created before the threshold date time
+//        List<User> unconfirmedUsers = userRepository.findByEnabledFalseAndCreatedAtBeforeAndDeletedAtIsNull(thresholdDateTime);
+//
+//        for (User user : unconfirmedUsers) {
+//            List<VerificationToken> userVerificationTokens = verificationTokenRepository.findByUserAndCreatedAtBefore(user, thresholdDateTime);
+//            verificationTokenRepository.deleteAll(userVerificationTokens);
+//
+//            List<Token> userTokens = tokenRepository.findAllByUser(user);
+//            tokenRepository.deleteAll(userTokens);
+//            userRepository.deleteAll(unconfirmedUsers);
+//        }
+//    }
 }
